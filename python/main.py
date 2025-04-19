fe4c0543a5d2117cc4b6063e242990e113938c62
XinBy <xinbaiying@icloud.com>
2025-04-19 22:10:41 +0800

    Change `seed+it` to `seed`

--- python/main.py
+++ python/main.py
@@ -105,7 +105,7 @@ def main():
             X=X, y=y,
             train_size=train_size,
             val_size=val_size,
-            seed=args.seed+it,
+            seed=args.seed,
             device=device
         )
 
